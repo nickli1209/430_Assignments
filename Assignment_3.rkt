eff549a422fee8ed9ec9e714f581a2295a91fa14
drewkim20 <102550676+drewkim20@users.noreply.github.com>
2024-04-22 15:54:09 -0700

    Update Assignment_3.rkt

--- Assignment_3.rkt
+++ Assignment_3.rkt
@@ -1,5 +1,26 @@
 #lang typed/racket
-(require typed/rackuint)
+(require typed/rackunit)
 ;;assingnment 3
 ;;not fully implemented
 
+
+(define-type ExprC (U Real Variable BinopC))
+(struct Binop ([op : Symbol] [left : ExprC] [right : ExprC])
+  #:transparent)
+(struct Variable ([name : Symbol])
+  #:transparent)
+
+;; (define (op-table [b : Binop]) : ExprC
+;;   (match b
+;;     [(Binop '+ l r)      ....]
+;;     [(Binop '- l r)      ....]
+;;     [(Binop '* l r)      ....]
+;;     [(Binop '/ l r)      ....]))
+
+(define (parse-expr [sexp : Sexp]) : ExprC
+  (match sexp
+    [()]))
+
+(define (parse-fundef [sexp : Sexp]) : FundefC)
+
+(define (parse-prog [sexp : Sexp]) : (Listof FundefC))
