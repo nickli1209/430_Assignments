d2f3b8ff7522f79af2dd257a40b0af6b6f8ec0e7
Nick Li <nmli@calpoly.edu>
2024-04-22 15:08:11 -0700

    Create lab_3.rkt

---
+++ Labs/lab_3.rkt
@@ -0,0 +1,54 @@
+#lang typed/racket
+(require typed/rackunit)
+;;Lab 3
+
+;;parse000-----------------------------------------------------------------------
+
+;;the function parse000 takes as input an s-expression, returns true if
+;;the s-expression is a list container a number, the symbol 'chris, and any symbol
+
+(define (parse000 [sex : Sexp]) : Boolean
+  (match sex
+    [(list n 'chris s)        (and (number? n) (symbol? s))]
+    [other                          #f]))
+
+;;tests
+(check-equal? (parse000 (list 10 'chris 'pp)) #t)
+(check-equal? (parse000 '(10 'chris 10)) #f)
+(check-equal? (parse000 (list 10 'chris 10)) #f) ;;ask why this fails...
+(check-equal? (parse000 '()) #f)
+
+
+;;parse001
+;;the function parse001 takes as input an s-expression, returns the symbol if
+;;the s-expression is a list containing a number, the symbol 'chris, and any symbol
+
+(define (parse001 [sex : Sexp]) : (U Symbol Boolean)
+  (match sex
+    [(list n 'chris s)        (if (and (number? n) (symbol? s)) s #f)]
+    [other                          #f]))
+
+;;tests
+(check-equal? (parse001 (list 10 'chris 'pp)) 'pp)
+(check-equal? (parse001 '(10 'chris 10)) #f)
+(check-equal? (parse001 (list 10 'chris 10)) #f) ;;ask why this fails...
+(check-equal? (parse001 '()) #f)
+
+;;parse002--------------------------------------------------------------------------
+;;the fucntion pasre002 takes an sexp as an input and returns a list l of numbers if the sexp
+;;was a list of len 3, where the second item is a list, otherwise false
+
+(define (parse002 [sex : Sexp]) :(U (Listof Real) Boolean)
+  (match sex
+    ;;list? check if of type list, andmap? on the list l, with procedure r=Real?
+    ;;checks if each element of l is a Real
+    [(list n l s)        (if (and (list? l) (andmap real? l)) l #f)] ;;ask how l becomes list of real instead of list of sexp
+    [other                          #f]))
+
+;;tests
+(check-equal? (parse002 (list 10 (list 1 2 3) 'pp)) (list 1 2 3))
+(check-equal? (parse001 '(10 'chris 10)) #f)
+(check-equal? (parse001 (list 10 'chris 10)) #f) ;;ask why this fails...
+(check-equal? (parse001 '()) #f)
+
+;;pa
