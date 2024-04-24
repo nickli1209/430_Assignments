a61337e974960297391a355c6ab2a8c6fe1f3d9d
Nick Li <nmli@calpoly.edu>
2024-04-23 21:49:22 -0700

    Update Assignment_3.rkt

--- Assignment_3.rkt
+++ Assignment_3.rkt
@@ -10,17 +10,26 @@
 (struct Variable ([name : Symbol])
   #:transparent)
 
+(define (parse-expr [sexp : Sexp]) : ExprC
+  (match sexp
+    ;;<num>
+    [(? real? n)    (NumC n)]
+    ;;{+ <expr> <expr>}
+    [(list '+ l r)     (PlusC (parse l) (parse r))]
+    ;;{* <expr> <expr>
+    [(list '* l r)     (MultC (parse l) (parse r))]
+    ;;syntax error
+    [other (error 'parse "invalid syntax. got: ~e" other)]))
+
+(define (parse-fundef [sexp : Sexp]) : FundefC)
+
+(define (parse-prog [sexp : Sexp]) : (Listof FundefC))
+
+
+;;worry about later at intepr time
 ;; (define (op-table [b : Binop]) : ExprC
 ;;   (match b
 ;;     [(Binop '+ l r)      ....]
 ;;     [(Binop '- l r)      ....]
 ;;     [(Binop '* l r)      ....]
 ;;     [(Binop '/ l r)      ....]))
-
-(define (parse-expr [sexp : Sexp]) : ExprC
-  (match sexp
-    [()]))
-
-(define (parse-fundef [sexp : Sexp]) : FundefC)
-
-(define (parse-prog [sexp : Sexp]) : (Listof FundefC))
