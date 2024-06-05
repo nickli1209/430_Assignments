4dc68a587cc12b9fe93843408bf09d536ec5cbaa
drewkim20 <102550676+drewkim20@users.noreply.github.com>
2024-06-04 20:16:14 -0700

    Update lab_6.rkt

--- Labs/lab_6.rkt
+++ Labs/lab_6.rkt
@@ -15,8 +15,57 @@
 ;;returns one random symbol from a set of 8 selected symbols
 (define syms '(a b c d e x y z))
 
-(define (random-symbol)
-  (list-ref syms (random 8)))
+(define (random-symbol) : Symbol
+  (list-ref syms (random (length syms))))
+
+;;random-base-term function
+;;generates a random expression
+;;chooses randomly from the set of non-referential expressions
+;;uses random-symbol to create the identifiers
+
+
+(define (random-base-term) : ExprC
+  (define idx (random 3))
+  (cond
+    [(= 0 idx)        (numC (random 100))]
+    [(= 1 idx)        (idC (random-symbol))]
+    [else             (strC (format "str~a" (random 100)))]))
+
+
+;;random-term function
+;;accepts a maximum depth, returns an expression tree not exceeding
+;;max depth
+
+(define (random-term [max-depth : Natural]) : ExprC
+  (if (= max-depth 0)
+      (random-base-term)
+      (let ([exp-idx (random 3)])
+         (cond
+           [(= exp-idx 0)     (ifC (random-term (- max-depth 1))
+                                   (random-term (- max-depth 1))
+                                   (random-term (- max-depth 1)))]
+           [(= exp-idx 1)     (let ([num-syms (random 4)])
+                                (lambC (make-syms num-syms) (random-term (- max-depth 1))))]
+           [else              (let ([num-args (random 4)])
+                                (appC (random-term (- max-depth 1)) (make-args num-args (- max-depth 1))))]))))
+
+;;random-term helper function
+;;generate args for lambs and function apps
+(define (make-syms [n : Natural]) : (Listof Symbol)
+  (cond
+    [(= n 0)      '()]
+    [else         (cons (random-symbol) (make-syms (- n 1)))]))
+
+(define (make-args [n : Natural] [depth : Natural]) : (Listof ExprC)
+  (cond
+    [(= n 0)      '()]
+    [else         (cons (random-term depth) (make-args (- n 1) depth))]))
+
+(random-term 3)
+
+;;unparse function here...
+
+
 
 
 
